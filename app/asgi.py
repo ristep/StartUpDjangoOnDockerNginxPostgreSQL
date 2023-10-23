14db4a75dd5b53af54634880f94b21c969afc19a
Rste Panovski <ristepan@gmail.com>
2023-10-23 13:31:20 +0200

    rebild dir structure app to top

--- app/asgi.py
+++ app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for main_project.
+ASGI config for app.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_asgi_application()
